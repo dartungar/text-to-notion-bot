053a41cc269ffd665536a8884954498f42f95c95
dartungar <dartungar@gmail.com>
2019-08-12 21:14:24 +0300

    exceptions and rudimentary checks

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from notion.block import TextBlock
 
 #tokens TODO delete Notion Noket from here
 BOT_TOKEN = os.environ['BOT_TOKEN']
-NOTION_TOKEN = os.environ['NOTION_TOKEN']
+NOTION_TOKEN = None #os.environ['NOTION_TOKEN']
 
 
 #create bot
@@ -22,6 +22,7 @@ def start_handler(message):
     bot.send_message(message.chat.id, 
         '''Hey there! 
         I\'m a deadpan simple bot for appending text to Notion page. 
+        Set your Notion Client with /setclient.
         Set page address with /setpage. 
         Then just send me text you want to appear in said Notion page.''')
 
@@ -29,23 +30,27 @@ def start_handler(message):
 
 @bot.message_handler(commands=['setclient'])
 def setclient_handler(message):
-    if not NOTION_TOKEN:
-        msg = bot.send_message(message.chat.id, 'please send me an Notion API key')
-        bot.register_next_step_handler(msg, get_notion_api_token)
-    global notion_client
-    notion_client = NotionClient(token_v2=NOTION_TOKEN)
-    bot.send_message(message.chat.id, 'Notion Client set!')
+        if message.from_user.username == 'dartungar':
+                NOTION_TOKEN = os.environ['NOTION_TOKEN']
+        if not NOTION_TOKEN:
+                msg = bot.send_message(message.chat.id, 'please send me an Notion API key')
+                bot.register_next_step_handler(msg, get_notion_api_token)
+        global notion_client
+        notion_client = NotionClient(token_v2=NOTION_TOKEN)
+        bot.send_message(message.chat.id, 'Notion Client set!')
 
 
 @bot.message_handler(commands=['setpage'])
 def setpage_handler(message):
-    msg = bot.send_message(message.chat.id, 'please send me an address of a page from your Notion', )
+    msg = bot.send_message(message.chat.id, 'please send me an address of a page from your Notion')
     bot.register_next_step_handler(msg, get_page_address)
     
 
 def get_notion_api_token(message):
     try:
         global NOTION_TOKEN
+        if not message.text:
+                raise Exception()
         NOTION_TOKEN = message.text
     except Exception as e:
         bot.send_message(message.chat.id, 'couldnt get Notion API!')
@@ -60,17 +65,26 @@ def get_page_address(message):
         bot.send_message(message.chat.id, 'coldnt set page!')
 
 
-def append_text_to_notion_page(client, page_address, text):
-    page = client.get_block(page_address)
-    newblock = page.children.add_new(TextBlock, title=text)
-
-
 @bot.message_handler(content_types=['text'])
 def text_handler(message):
-    text = message.text
-    append_text_to_notion_page(notion_client, page_address, text)
-    bot.send_message(message.chat.id, f'sent text to {page_address}!')
+        text = message.text
+        try:
+                if not notion_client:
+                        raise Exception('Notion Client not set!')
+                page = notion_client.get_block(page_address)
+                newblock = page.children.add_new(TextBlock, title=text)
+                bot.send_message(message.chat.id, f'sent text to {page.title}!')      
+        except Exception as e:
+                bot.send_message(message.chat.id, f'Error: {e.message}!')   
 
 
 
-bot.polling(none_stop=True, timeout=0)
+
+
+@bot.message_handler(content_types=['video', 'photo', 'sticker', 'voice', 'location'])
+def not_text_handler(message):
+    bot.send_message(message.chat.id, 'you can only send text (for now).')
+
+
+bot.polling(none_stop=True, timeout=100)
+
