cea2181766c35834032ce3fcc7eb88408e99bd05
dartungar <dartungar@gmail.com>
2019-08-12 21:49:47 +0300

    markup keyboard & /setpage command

--- bot.py
+++ bot.py
@@ -12,9 +12,17 @@ NOTION_TOKEN = None #os.environ['NOTION_TOKEN']
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-#globals TODO fix this shit
+#globals TODO fix this shit (with user dictionary?)
 page_address = None
 notion_client = None
+
+basic_keyboard = telebot.types.ReplyKeyboardMarkup()
+btn1 = telebot.types.KeyboardButton('/start')
+btn2 = telebot.types.KeyboardButton('/setclient')
+btn3 = telebot.types.KeyboardButton('/setpage')
+btn4 = telebot.types.KeyboardButton('/checkpage')
+basic_keyboard.add(btn1, btn2, btn3, btn4)
+
 
 
 @bot.message_handler(commands=['start', 'go', 'activate'])
@@ -24,7 +32,8 @@ def start_handler(message):
         I\'m a deadpan simple bot for appending text to Notion page. 
         Set your Notion Client with /setclient.
         Set page address with /setpage. 
-        Then just send me text you want to appear in said Notion page.''')
+        Then just send me text you want to appear in said Notion page.''', 
+        reply_markup=basic_keyboard)
 
 
 
@@ -37,14 +46,18 @@ def setclient_handler(message):
                 bot.register_next_step_handler(msg, get_notion_api_token)
         global notion_client
         notion_client = NotionClient(token_v2=NOTION_TOKEN)
-        bot.send_message(message.chat.id, 'Notion Client set!')
+        bot.send_message(message.chat.id, 'Notion Client set!', reply_markup=basic_keyboard)
 
 
 @bot.message_handler(commands=['setpage'])
 def setpage_handler(message):
     msg = bot.send_message(message.chat.id, 'please send me an address of a page from your Notion')
     bot.register_next_step_handler(msg, get_page_address)
-    
+
+
+@bot.message_handler(commands=['checkpage'])
+def checkpage_handler(message):
+        bot.send_message(message.chat.id, f'your page is set to: {page_address}', reply_markup=basic_keyboard)   
 
 def get_notion_api_token(message):
     try:
@@ -73,9 +86,9 @@ def text_handler(message):
                         raise Exception('Notion Client not set!')
                 page = notion_client.get_block(page_address)
                 newblock = page.children.add_new(TextBlock, title=text)
-                bot.send_message(message.chat.id, f'sent text to {page.title}!')      
+                bot.send_message(message.chat.id, f'sent text to {page.title}!', reply_markup=basic_keyboard)      
         except Exception as e:
-                bot.send_message(message.chat.id, f'Error while sending text to Notion: {e}!')   
+                bot.send_message(message.chat.id, f'Error while sending text to Notion: {e}!', reply_markup=basic_keyboard)   
 
 
 
@@ -83,7 +96,7 @@ def text_handler(message):
 
 @bot.message_handler(content_types=['video', 'photo', 'sticker', 'voice', 'location'])
 def not_text_handler(message):
-    bot.send_message(message.chat.id, 'you can only send text (for now).')
+    bot.send_message(message.chat.id, 'you can only send text (for now).', reply_markup=basic_keyboard)
 
 
 bot.polling(none_stop=True, timeout=100)
