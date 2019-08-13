6eb86c50ba7f5d0f1f82b7eaddccbb7c8cb35a07
dartungar <dartungar@gmail.com>
2019-08-13 07:37:43 +0300

    user dict v0 & changes to functions

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from notion.block import TextBlock
 
 #tokens TODO delete Notion Noket from here
 BOT_TOKEN = os.environ['BOT_TOKEN']
-NOTION_TOKEN = None #os.environ['NOTION_TOKEN']
+#NOTION_TOKEN = None #os.environ['NOTION_TOKEN']
 
 
 #create bot
@@ -15,8 +15,8 @@ bot = telebot.TeleBot(BOT_TOKEN)
 users = {}
 
 #globals TODO fix this shit (with user dictionary?)
-page_address = None
-notion_client = None
+#page_address = None
+#notion_client = None
 
 basic_keyboard = telebot.types.ReplyKeyboardMarkup(True)
 btn1 = telebot.types.KeyboardButton('/start')
@@ -29,28 +29,31 @@ basic_keyboard.add(btn1, btn2, btn3, btn4)
 # TODO: более элегантное решение с диктами
 @bot.message_handler(commands=['start', 'go', 'activate'])
 def start_handler(message):
-        username = message.from_user.username
-        users[username] = {'name': username}
-        bot.send_message(message.chat.id, 
-        f'''Hey there, {username}! 
-        I\'m a deadpan simple bot for appending text to Notion page. 
-        Set your Notion Client with /setclient.
-        Set page address with /setpage. 
-        Then just send me text you want to appear in said Notion page.''', 
-        reply_markup=basic_keyboard)
+    username = message.from_user.username
+    users[username] = {'name': username}
+    bot.send_message(message.chat.id, 
+    f'''Hey there, {username}! 
+    I\'m a deadpan simple bot for appending text to Notion page. 
+    Set your Notion Client with /setclient.
+    Set page address with /setpage. 
+    Then just send me text you want to appear in said Notion page.''', 
+    reply_markup=basic_keyboard)
 
 
 
 @bot.message_handler(commands=['setclient'])
 def setclient_handler(message):
-        if message.from_user.username == 'dartungar':
-                NOTION_TOKEN = os.environ['NOTION_TOKEN']
-        if not NOTION_TOKEN:
-                msg = bot.send_message(message.chat.id, 'please send me an Notion API key')
-                bot.register_next_step_handler(msg, get_notion_api_token)
-        global notion_client
-        notion_client = NotionClient(token_v2=NOTION_TOKEN)
-        bot.send_message(message.chat.id, 'Notion Client set!', reply_markup=basic_keyboard)
+    username = message.from_user.username
+    if username == 'dartungar':
+        users[username]['notion_api_token'] = os.environ['NOTION_TOKEN']
+    else:
+        if not users[username]['notion_api_token']:
+            msg = bot.send_message(message.chat.id, 'please send me an Notion API key')
+            bot.register_next_step_handler(msg, get_notion_api_token)
+    #global notion_client
+    #notion_client = NotionClient(token_v2=NOTION_TOKEN)
+    users[username]['notion_client'] = NotionClient(token_v2=users[username]['notion_api_token'])
+    bot.send_message(message.chat.id, 'Notion Client set!', reply_markup=basic_keyboard)
 
 
 @bot.message_handler(commands=['setpage'])
@@ -61,44 +64,49 @@ def setpage_handler(message):
 
 @bot.message_handler(commands=['checkpage'])
 def checkpage_handler(message):
-        try:
-                page_title = notion_client.get_block(page_address).title
-                page_icon = notion_client.get_block(page_address).icon
-                bot.send_message(message.chat.id, f'your page is set to: {page_icon}{page_title} ({page_address})', reply_markup=basic_keyboard)
-        except Exception as e:
-                bot.send_message(message.chat.id, f'Error while checking page: {e}!', reply_markup=basic_keyboard)
+    username = message.from_user.username
+    notion_client = users[username]['notion_client']
+    page_address = users[username]['page_address']
+    try:
+        users[username]['page_title'] = f'{notion_client.get_block(page_address).icon}{notion_client.get_block(page_address).title}'
+        bot.send_message(message.chat.id, f'your page is set to: {users[username]["page_title"]} ({users[username]["page_address"]})', reply_markup=basic_keyboard)
+    except Exception as e:
+            bot.send_message(message.chat.id, f'Error while checking page: {e}!', reply_markup=basic_keyboard)
 
 
 def get_notion_api_token(message):
     try:
-        global NOTION_TOKEN
         if not message.text:
-                raise Exception()
-        NOTION_TOKEN = message.text
+            raise Exception()
+        users[message.from_user.username]['notion_api_token'] = message.text
     except Exception as e:
-        bot.send_message(message.chat.id, 'couldnt get Notion API!')
+        bot.send_message(message.chat.id, 'couldnt set Notion API token!')
 
 
 def get_page_address(message):
     try:
-        global page_address
-        page_address = message.text
-        bot.send_message(message.chat.id, f'page set to {page_address}!')
+        users[message.from_user.username]["page_address"] = message.text
+        bot.send_message(message.chat.id, f'page set to {users[message.from_user.username]["page_address"]}!')
     except Exception as e:
         bot.send_message(message.chat.id, 'coldnt set page!')
 
 
 @bot.message_handler(content_types=['text'])
 def text_handler(message):
-        text = message.text
-        try:
-                if not notion_client:
-                        raise Exception('Notion Client not set!')
-                page = notion_client.get_block(page_address)
-                newblock = page.children.add_new(TextBlock, title=text)
-                bot.send_message(message.chat.id, f'sent text to {page.title}!', reply_markup=basic_keyboard)      
-        except Exception as e:
-                bot.send_message(message.chat.id, f'Error while sending text to Notion: {e}!', reply_markup=basic_keyboard)   
+    username = message.from_user.username
+    notion_client = users[username]['notion_client']
+    page_address = users[username]['page_address'] 
+    text = message.text
+    try:
+        if not users[username]['notion_client']:
+            raise Exception('Notion Client not set!')
+        if not users[username]['page_address']:
+            raise Exception('Page address not set!')
+        page = notion_client.get_block(page_address)
+        newblock = page.children.add_new(TextBlock, title=text)
+        bot.send_message(message.chat.id, f'sent text to {page.icon}{page.title}!', reply_markup=basic_keyboard)      
+    except Exception as e:
+        bot.send_message(message.chat.id, f'Error while sending text to Notion: {e}!', reply_markup=basic_keyboard)   
 
 
 
