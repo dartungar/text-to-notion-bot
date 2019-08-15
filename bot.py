2c6a841e5f3c491fc04b17410dc9155cc33424dc
dartungar <dartungar@gmail.com>
2019-08-15 22:03:56 +0300

    rewrite whole bot with python-tg-bot

--- bot.py
+++ bot.py
@@ -1,53 +1,38 @@
+import logging
 import os
-import telebot
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, RegexHandler, ConversationHandler, Filters, PicklePersistence
 from notion.client import NotionClient
 from notion.block import TextBlock
 
-#tokens TODO delete Notion Noket from here
-BOT_TOKEN = os.environ['BOT_TOKEN']
-#NOTION_TOKEN = None #os.environ['NOTION_TOKEN']
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
-#create bot
-bot = telebot.TeleBot(BOT_TOKEN)
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+if not BOT_TOKEN:
+    BOT_TOKEN = '979211903:AAHfUniyo_3F4s48alBSW4hwNZXj4_DhS24'
 
-#lame disct for storing users
-users = {}
+TYPING_NOTION_API_KEY, TYPING_NOTION_PAGE_ADDRESS = range(2)
 
-#globals TODO fix this shit (with user dictionary?)
-#page_address = None
-#notion_client = None
-
-basic_keyboard = telebot.types.ReplyKeyboardMarkup(True)
-btn1 = telebot.types.KeyboardButton('/start')
-btn2 = telebot.types.KeyboardButton('/help')
-btn3 = telebot.types.KeyboardButton('/setclient')
-btn4 = telebot.types.KeyboardButton('/checkclient')
-btn5 = telebot.types.KeyboardButton('/setpage')
-btn6 = telebot.types.KeyboardButton('/checkpage')
-basic_keyboard.row(btn1, btn2, btn3)
-basic_keyboard.row(btn4, btn5, btn6)
+keyboard = ReplyKeyboardMarkup([['/start', '/help', '/setclient'], ['/checkclient', '/setpage', '/checkpage']], True)
 
 
-# TODO: более элегантное решение с диктами
-@bot.message_handler(commands=['start', 'go', 'activate'])
-def start_handler(message):
-    username = message.from_user.username
-    users[username] = {'name': username}
-    bot.send_message(message.chat.id, 
-    f'''Hey there, {username}! 
-    I\'m a deadpan simple bot for appending text to Notion page.
+def start(update, context):
+    context.user_data['username'] = update.message.from_user.username
+    reply_text = f'''Hey there, {context.user_data['username']}! 
+    I\'m a deadpan simple context.bot for appending text to Notion page.
     Get your "Notion API key" (go to any page in your Notion.so and look for "token_v2" in cookies). 
     Set your Notion Client with /setclient.
     Set page address with /setpage. 
-    Then just send me text you want to appear on Notion page you set.''', 
-    reply_markup=basic_keyboard)
+    Then just send me text you want to appear on Notion page you set.'''
+    update.message.reply_text(reply_text, reply_markup=keyboard)
 
 
-@bot.message_handler(commands=['help'])
-def help_handler(message):
-    bot.send_message(message.chat.id, 
-    f'''In order to send stuff to Notion, you need to get your 'internal API key' for Notion.
+def help_msg(update, context):
+    reply_text = f'''In order to send stuff to Notion, you need to get your 'internal API key' for Notion.
 
     1. go to any of your notion.so pages in browser
     2. press F12 / open developer tools 
@@ -55,114 +40,101 @@ def help_handler(message):
     4. select https://www.notion.so
     5. find a cookie with name 'token_v2'
     6. copy its value
-    7. use /setclient command and pass token_v2 to this bot
+    7. use /setclient command and pass token_v2 to this context.bot
     8. choose Notion page to which you want send text, copy its URL
-    9. use /setpage and send URL to bot
+    9. use /setpage and send URL to context.bot
 
     Now any text you send to bot (except for commands) will be appended to Notion page you chose!
-    ''', 
-    reply_markup=basic_keyboard)
+    '''
+    update.message.reply_text(reply_text, reply_markup=keyboard)
 
 
-
-@bot.message_handler(commands=['setclient'])
-def setclient_handler(message):
-    username = message.from_user.username
-    if username == 'dartungar':
-        users[username]['notion_api_token'] = os.environ['NOTION_TOKEN']
-    else:
-        if not users[username].get('notion_api_token'):
-            msg = bot.send_message(message.chat.id, 'please send me an Notion API key')
-            bot.register_next_step_handler(msg, get_notion_api_token)
-    #global notion_client
-    #notion_client = NotionClient(token_v2=NOTION_TOKEN)
-    users[username]['notion_client'] = NotionClient(token_v2=users[username]['notion_api_token'])
-    bot.send_message(message.chat.id, 'Notion Client set!', reply_markup=basic_keyboard)
-
-
-@bot.message_handler(commands=['checkclient'])
-def checkclient_handler(message):
-    username = message.from_user.username
-    if users[username].get('notion_client'):
-        bot.send_message(message.chat.id, 'Notion Client OK.', reply_markup=basic_keyboard)
+def askclient(update, context):
+    if context.user_data['username'] == 'dartungar':
+        context.user_data['notion_api_token'] = os.environ['NOTION_TOKEN']
+        update.message.reply_text('welcome back, master Dartio!', reply_markup=keyboard)
         return
-    bot.send_message(message.chat.id, 'Notion Client not set.', reply_markup=basic_keyboard)    
+    if not context.user_data.get('notion_api_token'):
+        update.message.reply_text('please send me an Notion API key', reply_markup=keyboard)
+        return TYPING_NOTION_API_KEY
 
 
-@bot.message_handler(commands=['setpage'])
-def setpage_handler(message):
-    msg = bot.send_message(message.chat.id, 'please send me an address of a page from your Notion')
-    bot.register_next_step_handler(msg, get_page_address)
+def setclient(update, context):
+    context.user_data['notion_api_token'] = update.message.text
+    # TODO это вообще работает? :D
+    context.user_data['notion_client'] = NotionClient(token_v2=context.user_data['notion_api_token'])
+    update.message.reply_text('Notion Client set!', reply_markup=keyboard)
 
 
-@bot.message_handler(commands=['checkpage'])
-def checkpage_handler(message):
-    username = message.from_user.username
-    try:
-        page = get_page(message)
-        users[username]['page_title'] = f'{page.icon}{page.title}'
-        bot.send_message(message.chat.id, f'your page is set to: {users[username]["page_title"]} ({users[username]["page_address"]})', reply_markup=basic_keyboard)
-    except Exception as e:
-            bot.send_message(message.chat.id, f'Error while checking page: {e}', reply_markup=basic_keyboard)
+def askpage(update, context):
+    if not context.user_data.get('page_address'):
+        update.message.reply_text('please send me a URL of a page from your Notion.so', reply_markup=keyboard)
+        return TYPING_NOTION_PAGE_ADDRESS
 
 
-def get_notion_api_token(message):
-    try:
-        if not message.text:
-            raise Exception()
-        users[message.from_user.username]['notion_api_token'] = message.text
-    except Exception as e:
-        bot.send_message(message.chat.id, 'couldnt set Notion API token!')
-
-
-def get_page_address(message):
-    try:
-        users[message.from_user.username]["page_address"] = message.text
-        bot.send_message(message.chat.id, f'page set to {users[message.from_user.username]["page_address"]}!')
-    except Exception as e:
-        bot.send_message(message.chat.id, 'coldnt set page!')
-
-
-@bot.message_handler(content_types=['text'])
-def text_handler(message):
-    text = message.text
-    try:
-        page = get_page(message)
-        newblock = page.children.add_new(TextBlock, title=text)
-        bot.send_message(message.chat.id, f'sent text to {page.icon}{page.title}!', reply_markup=basic_keyboard)      
-    except Exception as e:
-        bot.send_message(message.chat.id, f'Error while sending text to Notion: {e}', reply_markup=basic_keyboard)   
-
-
-
-@bot.message_handler(content_types=['video', 'photo', 'sticker', 'voice', 'location'])
-def not_text_handler(message):
-    bot.send_message(message.chat.id, 'you can only send text (for now).', reply_markup=basic_keyboard)
-
-
-def get_page(message):
-    username = message.from_user.username
-    notion_client = users[username].get('notion_client')
-    page_address = users[username].get('page_address')
-
-    if not notion_client:
-        raise Exception('Notion Client not set!')
-    if not page_address:
-        raise Exception('Page address not set!')
-    
-    try:
-        page = notion_client.get_block(page_address)
-    except Exception:
-        raise Exception('Bad page URL!')
-
-    return page
-
-
-bot.polling(none_stop=True, timeout=100)
-
-
-
-
+def setpage(update, context):
+    page_address = update.message.text
+    context.user_data['page_address'] = page_address
+    notion_client = context.user_data['notion_client']
+    page = notion_client.get_block(page_address)
+    context.user_data['page'] = page
+    if page.icon:
+        context.user_data['page_title'] += page.icon
+    context.user_data['page_title'] += page.title
+    # TODO message from bot 
     
 
+def send_text_to_notion(update, context):
+    text = update.message.text
+    page = context.user_data['page']
+    newblock = page.children.add_new(TextBlock, title=text)
 
+
+def done(update, context):
+    update.message.reply_text('ok then.')
+    return ConversationHandler.END
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+
+
+def main():
+    pp = PicklePersistence(filename='notionbot')
+    updater = Updater(BOT_TOKEN, persistence=pp, use_context=True)
+
+    dp = updater.dispatcher
+
+    convhandler = ConversationHandler(
+        entry_points=[CommandHandler('start', start), 
+                        CommandHandler('setclient', askclient), 
+                        CommandHandler('setpage', askpage),],
+
+        states={
+            TYPING_NOTION_API_KEY: [MessageHandler(Filters.text, setclient)],
+            TYPING_NOTION_PAGE_ADDRESS: [MessageHandler(Filters.text, setpage)],  
+        },
+
+        fallbacks=[RegexHandler('^Done$', done)],
+        name='my_conversation',
+        persistent=True
+
+    )
+
+    dp.add_handler(convhandler)
+    help_handler = CommandHandler('help', help_msg)
+    dp.add_handler(help_handler)
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+
+
+if __name__ == '__main__':
+    main()
